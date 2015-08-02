93b7a835442a189e3458ba37e349a94ed1754593
Fumihiko Nishio <fumihiko.nishio+github@gmail.com>
2015-08-02 11:50:57 +0900

    Add LED flash code (device2.py)

--- device2.py
+++ device2.py
@@ -1,9 +1,10 @@
 ### MQTT client 2
 ###
-### 1. Send incremental counter event periodecally.
-### 2. When receiving toggle command, reset counter to the "count" value.
+### 1. subscirbe LED toggle command
+### 2. In receiving toggle command, toggle LED.
 
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
 import time
 import json
 
@@ -13,22 +14,30 @@ CLIENT_ID_FORMAT = "d:{org_id}:{type_id}:{device_id}"
 DEVICE_TYPE = "MQTTDevice"
 USERNAME = "use-token-auth"
 ENDPOINT_FORMAT = "{org_id}.messaging.internetofthings.ibmcloud.com"
-EVENT_TOPIC_FORMAT = "iot-2/evt/{event_id}/fmt/json"
 COMMAND_TOPIC_FORMAT = "iot-2/cmd/{command_id}/fmt/json"
+LED_PIN = 25
+on_off = False
 
-# incremental counter
-step = 1
+def gpio_init():
+    global on_off
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup (LED_PIN, GPIO.OUT)
+    GPIO.output(LED_PIN, on_off)
 
 def on_connect(client, userdata, flags, rc):
-#    print("Connected with result code "+str(rc))
-    client.subscribe(COMMAND_TOPIC_FORMAT.format(command_id="cid"))
+    print("Connected with result code "+str(rc))
+    client.subscribe(COMMAND_TOPIC_FORMAT.format(command_id="led"))
 
 def on_message(client, userdata, msg):
-    global step
-    print(msg.topic+" "+str(msg.payload))
     payload = json.loads(msg.payload.decode())
     if payload['cmd'] == "toggle":
-       step = -step
+       print(msg.topic+" "+str(msg.payload))
+       # control GPIO
+       global on_off
+       on_off = ~on_off
+       GPIO.output(LED_PIN, on_off)
+
+gpio_init()
 
 client_id = CLIENT_ID_FORMAT.format(
     org_id=c.ORG_ID, type_id=DEVICE_TYPE, device_id=c.DEVICE_ID)
@@ -40,11 +49,4 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.connect(endpoint, 1883)
 
-count = 0
-while client.loop() == 0:
-    msg = json.dumps({ "d" : { "count" : count } });
-    client.publish(EVENT_TOPIC_FORMAT.format(event_id="eid"),
-        msg, 0, True)
-    print("sent: " + msg)
-    time.sleep(1.5)
-    count = count + step
+client.loop_forever()
